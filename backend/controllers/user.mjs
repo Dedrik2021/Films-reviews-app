cea1d2618ff9ded792e0690d5a25019b251347c2
Oleg Grunski <oleggr578@gmail.com>
2023-06-17 23:17:33 +0200

    maked email verification

--- backend/controllers/user.mjs
+++ backend/controllers/user.mjs
@@ -1,15 +1,54 @@
-import User from '../models/user.mjs'
+import nodemailer from 'nodemailer'
+
+import User from '../models/user.mjs';
+import EmailVerificationToken from '../models/emailVerificationToken.mjs';
 
 const create = async (req, res) => {
-    const {name, email, password} = req.body
+	const { name, email, password } = req.body;
 
-    const oldUser = await User.findOne({email})
-    if (oldUser) return res.status(401).json({error: "This email already in use!"})
+	const oldUser = await User.findOne({ email });
+	if (oldUser) return res.status(401).json({ error: 'This email already in use!' });
 
-    const newUser = new User({name, email, password})
-    await newUser.save()
+	const newUser = new User({ name, email, password });
+	await newUser.save();
 
-	res.status(201).json({user: newUser});
+	// generate 6 digit otp
+	let OTP = '';
+	for (let i = 0; i < 5; i++) {
+		const randomVal = Math.round(Math.random() * 9);
+		OTP += randomVal;
+	}
+
+	// store otp inside our db
+	const newEmailVerificationToken = new EmailVerificationToken({
+		owner: newUser._id,
+		token: OTP,
+	});
+	await newEmailVerificationToken.save();
+
+	//send that otp to our user
+	const transport = nodemailer.createTransport({
+		host: 'sandbox.smtp.mailtrap.io',
+		port: 2525,
+		auth: {
+			user: '42a0dc509c2bde',
+			pass: '********6388',
+		},
+	});
+
+	transport.sendMail({
+		from: 'verification@reviewapp.com',
+		to: newUser.email,
+		subject: 'Email Verification',
+		html: `
+            <p>Your verification OTP</p>
+            <h1>${OTP}</h1>
+        `,
+	});
+
+	res.status(201).json({
+		message: 'Please verify your email. OTP has been sent to your email address!',
+	});
 };
 
 export { create };
---
+++ backend/models/emailVerificationToken.mjs
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const emailVerificationTokenSchema = mongoose.Schema({
+	owner: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		required: true,
+	},
+	token: {
+		type: String,
+		required: true,
+	},
+	createAt: {
+		type: Date,
+		expires: 3600,
+		default: Date.now(),
+	},
+});
+
+emailVerificationTokenSchema.pre('save', async function (next) {
+	if (this.isModified('token')) {
+		this.token = await bcrypt.hash(this.token, 10);
+	}
+	next();
+});
+
+export default mongoose.model('EmailVerificationToken', emailVerificationTokenSchema);
