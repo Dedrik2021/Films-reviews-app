87a9d7dd01033067f00769419e6bfb9e4c346450
Oleg Grunski <oleggr578@gmail.com>
2023-06-25 18:53:19 +0200

    create submit form in Signup

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,3 +1,5 @@
+import {useState} from 'react'
+
 import Container from '../Container';
 import Title from '../form/Title';
 import FormInput from '../form/FormInput';
@@ -7,25 +9,50 @@ import { commonModelClasses } from '../../utils/theme';
 import FormContainer from '../form/FormContainer';
 
 const Signup = () => {
+    const [userInfo, setUserInfo] = useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+
+    const {name, email, password} = userInfo
+
+    const handleChange = ({target}) => {
+        const {value, name} = target
+        setUserInfo({...userInfo, [name]: value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(userInfo);
+    }
+
 	return (
 		<FormContainer>
 			<Container>
-				<form className={commonModelClasses}>
+				<form onSubmit={handleSubmit} className={commonModelClasses}>
 					<Title>Sign Up</Title>
 					<FormInput 
                         placeholder="Jhon Doe"
                         name="name"
                         labelName="Name"
+                        value={name}
+                        onChange={handleChange}
                     />
 					<FormInput 
                         placeholder="Jhon@gmail.com"
                         name="email"
                         labelName="Email"
+                        value={email}
+                        onChange={handleChange}
                     />
 					<FormInput 
                         placeholder="********"
                         name="password"
                         labelName="Password"
+                        value={password}
+                        type='password'
+                        onChange={handleChange}
                     />
                     <SubmitBtn>Sign Up</SubmitBtn>
                     <div className="flex justify-between">
