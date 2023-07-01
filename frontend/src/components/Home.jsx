40db759471cbc7e7bb6d9e46da12fa450ada6f03
Oleg Grunski <oleggr578@gmail.com>
2023-07-01 17:04:57 +0200

    Home component refactor

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,28 +1,9 @@
-import {useNavigate} from 'react-router-dom'
-
-import { useAuth } from '../hooks';
-import Container from './Container';
+import NotVerified from './user/NotVerified';
 
 const Home = () => {
-    const navigate = useNavigate()
-
-	const { authInfo } = useAuth();
-    const {isLoggedIn} = authInfo
-    const isVerified = authInfo.profile?.isVerified
-
-    const navigateToVerification = () => {
-        navigate('/auth/verification', {state: {user: authInfo.profile}})
-    }
 
 	return (
-		<Container>
-			{isLoggedIn && !isVerified ? (
-                <p className='text-lg text-center bg-blue-50 p-2'>
-				It looks like you haven't verified your accont,{' '}
-				<button onClick={navigateToVerification} className='text-blue-500 font-semibold hover:underline' type="button">Click here to verified your account</button>
-			</p>
-            ) : null}
-		</Container>
+		<NotVerified/>
 	);
 };
 
---
+++ frontend/src/components/user/NotVerified.jsx
@@ -0,0 +1,29 @@
+import {useNavigate} from 'react-router-dom'
+
+import Container from "../Container";
+import { useAuth } from '../../hooks';
+
+const NotVerified = () => {
+	const navigate = useNavigate()
+
+	const { authInfo } = useAuth();
+    const {isLoggedIn} = authInfo
+    const isVerified = authInfo.profile?.isVerified
+
+    const navigateToVerification = () => {
+        navigate('/auth/verification', {state: {user: authInfo.profile}})
+    }
+
+	return (
+		<Container>
+			{isLoggedIn && !isVerified ? (
+                <p className='text-lg text-center bg-blue-50 p-2'>
+				It looks like you haven't verified your accont,{' '}
+				<button onClick={navigateToVerification} className='text-blue-500 font-semibold hover:underline' type="button">Click here to verified your account</button>
+			</p>
+            ) : null}
+		</Container>
+	);
+};
+
+export default NotVerified;
