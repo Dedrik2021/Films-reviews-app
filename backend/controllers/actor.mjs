6deac8fc2c83e2a4db4856a3eb1edb4c1e079f8f
Oleg Grunski <oleggr578@gmail.com>
2023-07-03 00:12:00 +0200

    create updateActor controller

--- backend/controllers/actor.mjs
+++ backend/controllers/actor.mjs
@@ -1,7 +1,9 @@
 import { v2 as cloudinary } from 'cloudinary';
 import dotenv from 'dotenv';
+import {isValidObjectId} from 'mongoose'
 
 import Actor from '../models/actor.mjs';
+import { sendError } from '../utils/helper.mjs';
 
 dotenv.config();
 
@@ -27,4 +29,37 @@ const createActor = async (req, res) => {
 	res.status(201).json({ id: newActor._id, name, about, gender, avatar: newActor.avatar?.url });
 };
 
-export { createActor };
+const updateActor = async (req, res) => {
+    const {name, about, gender} = req.body
+
+    const {file} = req
+    const {actorId} = req.params
+
+    if (!isValidObjectId(actorId)) return sendError(res, "Invalid request!")
+
+    const actor = await Actor.findById(actorId)
+    if (!actor) return sendError(res, "Invalid request, record not found!")
+
+    const public_id = actor.avatar?.public_id
+
+    if (public_id && file) {
+        const {result} = await cloudinary.uploader.destroy(public_id)
+
+        if (result !== 'ok') return sendError(res, "Could not remove image from cloud!")
+    }
+
+    if (file) {
+		const { secure_url, public_id } = await cloudinary.uploader.upload(file.path);
+		actor.avatar = { url: secure_url, public_id };
+	}
+
+    actor.name = name
+    actor.about = about
+    actor.gender = gender
+
+    await actor.save()
+
+    res.status(201).json({ id: actor._id, name, about, gender, avatar: actor.avatar?.url });
+}
+
+export { createActor, updateActor };
--- backend/routes/actor.mjs
+++ backend/routes/actor.mjs
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
-import { createActor } from '../controllers/actor.mjs';
+import { createActor, updateActor } from '../controllers/actor.mjs';
 import { uploadImage } from '../middlewares/multer.mjs';
 import { validatorActorInfo, validate } from '../middlewares/validator.mjs';
 
 const router = Router();
 
 router.post('/create', uploadImage.single('avatar'), validatorActorInfo, validate, createActor);
+router.post('/update/:actorId', uploadImage.single('avatar'), validatorActorInfo, validate, updateActor);
 
 export default router;
