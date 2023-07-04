84a6be61cbdf85f6f0687291b7df74ba360da43a
Oleg Grunski <oleggr578@gmail.com>
2023-07-04 23:44:51 +0200

    added getLatestActor controller

--- backend/controllers/actor.mjs
+++ backend/controllers/actor.mjs
@@ -1,6 +1,6 @@
 import { v2 as cloudinary } from 'cloudinary';
 import dotenv from 'dotenv';
-import {isValidObjectId} from 'mongoose'
+import { isValidObjectId } from 'mongoose';
 
 import Actor from '../models/actor.mjs';
 import { sendError } from '../utils/helper.mjs';
@@ -22,8 +22,11 @@ const createActor = async (req, res) => {
 
 	if (file) {
 		const { secure_url, public_id } = await cloudinary.uploader.upload(file.path, {
-            gravity: "face", height: 500, width: 500, crop: "thumb"
-        });
+			gravity: 'face',
+			height: 500,
+			width: 500,
+			crop: 'thumb',
+		});
 		newActor.avatar = { url: secure_url, public_id };
 	}
 	await newActor.save();
@@ -32,66 +35,76 @@ const createActor = async (req, res) => {
 };
 
 const updateActor = async (req, res) => {
-    const {name, about, gender} = req.body
+	const { name, about, gender } = req.body;
 
-    const {file} = req
-    const {actorId} = req.params
+	const { file } = req;
+	const { actorId } = req.params;
 
-    if (!isValidObjectId(actorId)) return sendError(res, "Invalid request!")
+	if (!isValidObjectId(actorId)) return sendError(res, 'Invalid request!');
 
-    const actor = await Actor.findById(actorId)
-    if (!actor) return sendError(res, "Invalid request, record not found!")
+	const actor = await Actor.findById(actorId);
+	if (!actor) return sendError(res, 'Invalid request, record not found!');
 
-    const public_id = actor.avatar?.public_id
+	const public_id = actor.avatar?.public_id;
 
-    if (public_id && file) {
-        const {result} = await cloudinary.uploader.destroy(public_id)
+	if (public_id && file) {
+		const { result } = await cloudinary.uploader.destroy(public_id);
 
-        if (result !== 'ok') return sendError(res, "Could not remove image from cloud!")
-    }
+		if (result !== 'ok') return sendError(res, 'Could not remove image from cloud!');
+	}
 
-    if (file) {
+	if (file) {
 		const { secure_url, public_id } = await cloudinary.uploader.upload(file.path, {
-            gravity: "face", height: 500, width: 500, crop: "thumb"
-        });
+			gravity: 'face',
+			height: 500,
+			width: 500,
+			crop: 'thumb',
+		});
 		actor.avatar = { url: secure_url, public_id };
 	}
 
-    actor.name = name
-    actor.about = about
-    actor.gender = gender
+	actor.name = name;
+	actor.about = about;
+	actor.gender = gender;
 
-    await actor.save()
+	await actor.save();
 
-    res.status(201).json({ id: actor._id, name, about, gender, avatar: actor.avatar?.url });
-}
+	res.status(201).json({ id: actor._id, name, about, gender, avatar: actor.avatar?.url });
+};
 
 const removeActor = async (req, res) => {
-    const {actorId} = req.params
+	const { actorId } = req.params;
 
-    if (!isValidObjectId(actorId)) return sendError(res, "Invalid request!")
+	if (!isValidObjectId(actorId)) return sendError(res, 'Invalid request!');
 
-    const actor = await Actor.findById(actorId)
-    if (!actor) return sendError(res, "Invalid request, record not found!")
+	const actor = await Actor.findById(actorId);
+	if (!actor) return sendError(res, 'Invalid request, record not found!');
 
-    const public_id = actor.avatar?.public_id
+	const public_id = actor.avatar?.public_id;
 
-    if (public_id) {
-        const {result} = await cloudinary.uploader.destroy(public_id)
+	if (public_id) {
+		const { result } = await cloudinary.uploader.destroy(public_id);
 
-        if (result !== 'ok') return sendError(res, "Could not remove image from cloud!")
-    }
+		if (result !== 'ok') return sendError(res, 'Could not remove image from cloud!');
+	}
 
-    await Actor.findByIdAndDelete(actorId)
+	await Actor.findByIdAndDelete(actorId);
 
-    res.status(201).json({message: "Actor has been removed seccessfully!"})
-}
+	res.status(201).json({ message: 'Actor has been removed seccessfully!' });
+};
 
 const searchActor = async (req, res) => {
-    const {query} = req
-    const result = await Actor.find({$text: {$search: `"${query.name}"`}})
+	const { query } = req;
+	const result = await Actor.find({ $text: { $search: `"${query.name}"` } });
+
+	res.status(201).json(result);
+};
+
+const getLatestActors = async (req, res) => {
+    const result = await Actor.find().sort({ceratedAt: '-1'}).limit(12)
 
     res.status(201).json(result)
-}
+};
 
-export { createActor, updateActor, removeActor, searchActor };
+
+export { createActor, updateActor, removeActor, searchActor, getLatestActors };
--- backend/routes/actor.mjs
+++ backend/routes/actor.mjs
@@ -1,15 +1,29 @@
 import { Router } from 'express';
 
-import { createActor, updateActor, removeActor, searchActor } from '../controllers/actor.mjs';
+import {
+	createActor,
+	updateActor,
+	removeActor,
+	searchActor,
+	getLatestActors
+} from '../controllers/actor.mjs';
 import { uploadImage } from '../middlewares/multer.mjs';
 import { validatorActorInfo, validate } from '../middlewares/validator.mjs';
 
 const router = Router();
 
 router.post('/create', uploadImage.single('avatar'), validatorActorInfo, validate, createActor);
-router.post('/update/:actorId', uploadImage.single('avatar'), validatorActorInfo, validate, updateActor);
+router.post(
+	'/update/:actorId',
+	uploadImage.single('avatar'),
+	validatorActorInfo,
+	validate,
+	updateActor,
+);
 
-router.delete('/:actorId', removeActor)
-router.get('/search', searchActor)
+router.delete('/:actorId', removeActor);
+router.get('/search', searchActor);
+router.get('/latest-uploads', getLatestActors);
+
 
 export default router;
