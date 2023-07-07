5a25afc0b864fd78104d82ab26ddfa0648eebd53
Oleg Grunski <oleggr578@gmail.com>
2023-07-07 22:52:48 +0200

    create uploadTrailer controller

--- backend/controllers/movie.mjs
+++ backend/controllers/movie.mjs
@@ -9,7 +9,14 @@ import { formatActor } from '../utils/helper.mjs';
 dotenv.config();
 
 const uploadTrailer = async (req, res) => {
+    const {file} = req
 
+    if (!file) return sendError(res, "Video file is missing!")
+
+    const {secure_url: url, public_id} = await cloudinary.uploader.upload(file.path, {
+        resource_type: "video"
+    })
+    res.status(201).json({url, public_id})
 }
 
 export { uploadTrailer };
