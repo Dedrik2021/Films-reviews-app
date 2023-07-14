ea5bee2e341126e258a992badd66b6dce433a920
Oleg Grunski <oleggr578@gmail.com>
2023-07-15 00:25:47 +0200

    maked admin Nav visible everywhere

---
+++ frontend/src/components/admin/Navbar.jsx
@@ -0,0 +1,66 @@
+import { Link, NavLink } from 'react-router-dom';
+import { AiOutlineHome } from 'react-icons/ai';
+import { BiMoviePlay } from 'react-icons/bi';
+import { FaUserNinja } from 'react-icons/fa';
+import { FiLogOut } from 'react-icons/fi';
+
+const Navbar = () => {
+	return (
+		<nav className="w-48 min-h-screen bg-secondary border-r border-gray-300">
+			<div className=" flex flex-col justify-between sticky top-0 h-screen pl-5 pt-1 pb-5 ">
+				<div>
+					<Link to="/" className="mb-8 block">
+						<img className="h-14 p-2" src="../logo.png" alt="logo" />
+					</Link>
+					<ul>
+						<li>
+							<NavItem to="/">
+								<AiOutlineHome />
+								<span>Home</span>
+							</NavItem>
+						</li>
+						<li>
+							<NavItem to="/movies">
+								<BiMoviePlay />
+								<span>Movies</span>
+							</NavItem>
+						</li>
+						<li>
+							<NavItem to="/actors">
+								<FaUserNinja />
+								<span>Actors</span>
+							</NavItem>
+						</li>
+					</ul>
+				</div>
+				<div className=" flex flex-col items-start">
+					<span className="font-semibold text-white text-xl">Admin</span>
+					<button
+						type="button"
+						className="flex items-center space-x-2 text-dark-subtle text-sm hover:text-white transition"
+					>
+						<FiLogOut />
+						<span>Log out</span>
+					</button>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+const NavItem = ({ children, to }) => {
+	return (
+		<NavLink
+			to={to}
+			className={({ isActive }) =>
+				`flex items-center text-lg space-x-2 p-2 hover:opacity-80 transition ${
+					isActive ? 'text-white' : 'text-gray-400'
+				}`
+			}
+		>
+			{children}
+		</NavLink>
+	);
+};
+
+export default Navbar;
--- frontend/src/navigator/AdminNavigator.jsx
+++ frontend/src/navigator/AdminNavigator.jsx
@@ -4,16 +4,22 @@ import Dashboard from '../components/admin/Dashboard';
 import Movies from '../components/admin/Movies';
 import Actors from '../components/admin/Actors';
 import NotFound from '../components/NotFound';
+import Navbar from '../components/admin/Navbar';
 
 const AdminNavigator = () => {
 	return (
-		<Routes>
-			<Route path="/" element={<Dashboard />} />
-			<Route path="/movies" element={<Movies />} />
-			<Route path="/actors" element={<Actors />} />
+		<div className="flex">
+			<Navbar />
+			<div className="flex-1 p-2 max-w-screen-xl">
+				<Routes>
+					<Route path="/" element={<Dashboard />} />
+					<Route path="/movies" element={<Movies />} />
+					<Route path="/actors" element={<Actors />} />
 
-			<Route path="*" element={<NotFound />} />
-		</Routes>
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</div>
+		</div>
 	);
 };
 
