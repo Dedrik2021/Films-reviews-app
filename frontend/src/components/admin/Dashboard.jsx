3642b504ed600fdb17af066ecb538fced2877090
Oleg Grunski <oleggr578@gmail.com>
2023-07-14 17:54:32 +0200

    added logout button

--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -2,34 +2,43 @@ import { Link, NavLink } from 'react-router-dom';
 import { AiOutlineHome } from 'react-icons/ai';
 import { BiMoviePlay } from 'react-icons/bi';
 import { FaUserNinja } from 'react-icons/fa';
+import { FiLogOut } from 'react-icons/fi';
 
 const Dashboard = () => {
 	return (
-		<nav className="w-48 min-h-screen bg-secondary border-r border-gray-300 pl-5">
-			<Link to="/">
-				<img className="h-14 p-2" src="../logo.png" alt="logo" />
-			</Link>
-
-			<ul>
-				<li>
-					<NavItem to="/">
-						<AiOutlineHome />
-						<span>Home</span>
-					</NavItem>
-				</li>
-				<li>
-					<NavItem to="/movies">
-						<BiMoviePlay />
-						<span>Movies</span>
-					</NavItem>
-				</li>
-				<li>
-					<NavItem to="/actors">
-						<FaUserNinja />
-						<span>Actor</span>
-					</NavItem>
-				</li>
-			</ul>
+		<nav className="w-48 min-h-screen bg-secondary border-r border-gray-300 pl-5 pt-1 pb-5 flex flex-col justify-between">
+			<div>
+				<Link to="/" className='mb-8 block'>
+					<img className="h-14 p-2" src="../logo.png" alt="logo" />
+				</Link>
+				<ul>
+					<li>
+						<NavItem to="/">
+							<AiOutlineHome />
+							<span>Home</span>
+						</NavItem>
+					</li>
+					<li>
+						<NavItem to="/movies">
+							<BiMoviePlay />
+							<span>Movies</span>
+						</NavItem>
+					</li>
+					<li>
+						<NavItem to="/actors">
+							<FaUserNinja />
+							<span>Actors</span>
+						</NavItem>
+					</li>
+				</ul>
+			</div>
+			<div className=" flex flex-col items-start">
+				<span className='font-semibold text-white text-xl'>Admin</span>
+				<button type="button" className="flex items-center space-x-2 text-dark-subtle text-sm hover:text-white transition">
+					<FiLogOut />
+					<span>Log out</span>
+				</button>
+			</div>
 		</nav>
 	);
 };
