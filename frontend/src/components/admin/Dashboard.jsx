c138240eeea46876efd9dc08f568a5b0f3353a23
Oleg Grunski <oleggr578@gmail.com>
2023-07-16 16:18:28 +0200

    added togling Options modal

--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -1,6 +1,9 @@
 import { AiOutlinePlus } from 'react-icons/ai';
+import { useState } from 'react';
 
 const Dashboard = () => {
+	const [showOptions, setShowOptions] = useState(false)
+
 	return (
 		<div className="flex items-center justify-between relative">
 			<div>
@@ -13,17 +16,19 @@ const Dashboard = () => {
 				/>
 			</div>
 
-			<button className="flex items-center space-x-2 border-secondary hover:border-primary text-secondary hover:opacity-80 transition font-semibold border-2 p-1 rounded text-lg px-2 py-1">
+			<button onClick={() => setShowOptions(!showOptions)} className="flex items-center space-x-2 border-secondary hover:border-primary text-secondary hover:opacity-80 transition font-semibold border-2 p-1 rounded text-lg px-2 py-1">
 				<span>Create</span>
 				<AiOutlinePlus />
 			</button>
 
-			<CreateOptions />
+			<CreateOptions visible={showOptions} />
 		</div>
 	);
 };
 
-const CreateOptions = () => {
+const CreateOptions = ({visible}) => {
+	if (!visible) return null
+
 	return (
 		<div className="absolute right-0 top-12 flex flex-col space-y-3 p-5 dark:bg-secondary bg-white drop-shadow-lg">
 			<Option>Add Movie</Option>
