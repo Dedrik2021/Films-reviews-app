75e215cb03375606eb04d806d41a3b0c97e6cee3
Oleg Grunski <oleggr578@gmail.com>
2023-07-17 23:49:55 +0200

    added closing modal on click outside

--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -1,8 +1,8 @@
 import { AiOutlinePlus } from 'react-icons/ai';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const Dashboard = () => {
-	const [showOptions, setShowOptions] = useState(false)
+	const [showOptions, setShowOptions] = useState(false);
 
 	return (
 		<div className="flex items-center justify-between relative">
@@ -16,7 +16,10 @@ const Dashboard = () => {
 				/>
 			</div>
 
-			<button onClick={() => setShowOptions(!showOptions)} className="flex items-center space-x-2 border-secondary hover:border-primary text-secondary hover:opacity-80 transition font-semibold border-2 p-1 rounded text-lg px-2 py-1">
+			<button
+				onClick={() => setShowOptions(!showOptions)}
+				className="flex items-center space-x-2 border-secondary hover:border-primary text-secondary hover:opacity-80 transition font-semibold border-2 p-1 rounded text-lg px-2 py-1"
+			>
 				<span>Create</span>
 				<AiOutlinePlus />
 			</button>
@@ -26,11 +29,14 @@ const Dashboard = () => {
 	);
 };
 
-const CreateOptions = ({visible}) => {
-	if (!visible) return null
+const CreateOptions = ({ visible }) => {
+
+	if (!visible) return null;
 
 	return (
-		<div className="absolute right-0 top-12 flex flex-col space-y-3 p-5 dark:bg-secondary bg-white drop-shadow-lg animate-scale">
+		<div
+			className="absolute right-0 top-12 flex flex-col space-y-3 p-5 dark:bg-secondary bg-white drop-shadow-lg animate-scale"
+		>
 			<Option>Add Movie</Option>
 			<Option>Add Actor</Option>
 		</div>
