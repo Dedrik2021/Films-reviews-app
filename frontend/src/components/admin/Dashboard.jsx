ec6e48492c897ab0566eff553546f69a509640a1
Oleg Grunski <oleggr578@gmail.com>
2023-07-20 14:09:25 +0200

    added component to display progress component

--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -1,13 +1,7 @@
 import MovieUpload from './MovieUpload';
 
 const Dashboard = () => {
-	return (
-		<div className="fixed inset-0 dark:bg-white dark:bg-opacity-50 bg-primary bg-opacity-50 backdrop-blur-sm flex items-center justify-center">
-			<div className="dark:bg-primary bg-white rounded w-[45rem] h-[40rem] overflow-auto ">
-				<MovieUpload />
-			</div>
-		</div>
-	);
+	return <MovieUpload />;
 };
 
 export default Dashboard;
--- frontend/src/components/admin/MovieUpload.jsx
+++ frontend/src/components/admin/MovieUpload.jsx
@@ -22,11 +22,28 @@ const MovieUpload = () => {
 	};
 
 	return (
-		<TrailerSelector
-			onTypeError={handleTypeError}
-			handleChange={handleChange}
-			visible={!videoSelected}
-		/>
+		<div className="fixed inset-0 dark:bg-white dark:bg-opacity-50 bg-primary bg-opacity-50 backdrop-blur-sm flex items-center justify-center">
+			<div className="dark:bg-primary bg-white rounded w-[45rem] h-[40rem] overflow-auto ">
+				<div className="p-2">
+					<div className="dark:bg-secondary bg-white drop-shadow-lg rounded p-3">
+						<div className="h-3 dark:bg-dark-subtle bg-light-subtle relative overflow-hidden">
+							<div
+								style={{ width: '80%' }}
+								className={`h-full absolute left-0 dark:bg-white bg-secondary`}
+							></div>
+						</div>
+						<p className="font-semibold dark:text-dark-subtle text-light-subtle animate-pulse mt-2">
+							Upload progress 80%
+						</p>
+					</div>
+				</div>
+				<TrailerSelector
+					onTypeError={handleTypeError}
+					handleChange={handleChange}
+					visible={!videoSelected}
+				/>
+			</div>
+		</div>
 	);
 };
 
