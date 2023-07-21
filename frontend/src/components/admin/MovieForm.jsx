ade9934c901316d703ed07b5e8416b4bdd1fc602
Oleg Grunski <oleggr578@gmail.com>
2023-07-22 00:38:22 +0200

    refactoring classes for input field

--- frontend/src/components/admin/MovieForm.jsx
+++ frontend/src/components/admin/MovieForm.jsx
@@ -1,3 +1,6 @@
+const commonInputClasses =
+	'w-full bg-transparent outline-none dark:border-dark-subtle border-light-subtle dark:focus:border-white focus:border-primary transition dark:text-white text-primary';
+
 const MovieForm = () => {
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -5,17 +8,35 @@ const MovieForm = () => {
 
 	return (
 		<form onSubmit={handleSubmit} className="flex space-x-3">
-			<div className="w-[70%] h-5">
-				<label htmlFor="tile" className="dark:text-dark-subtle text-light-subtle font-semibold">Title</label>
-				<input
-					id="title"
-					type="text"
-					className="w-full bg-transparent outline-none border-b-2 dark:border-dark-subtle border-light-subtle dark:focus:border-white focus:border-primary transition dark:text-white text-primary font-semibold text-xl p-1"
-                    placeholder="Title"
-				/>
+			<div className="w-[70%] h-5 space-y-5">
+				<div>
+					<Label htmlFor="title">Title</Label>
+					<input
+						id="title"
+						type="text"
+						className={`${commonInputClasses} border-b-2 font-semibold text-xl p-1`}
+						placeholder="Title"
+					/>
+				</div>
+				<div>
+					<Label htmlFor="storyLine">Story Line</Label>
+					<textarea
+						id="storyLine"
+						className={`${commonInputClasses} resize-none h-24 border-b-2`}
+						placeholder="Movie story line"
+					></textarea>
+				</div>
 			</div>
 			<div className="w-[30%] h-5 bg-blue-400"></div>
 		</form>
+	);
+};
+
+const Label = ({ children, htmlFor }) => {
+	return (
+		<label htmlFor={htmlFor} className="dark:text-dark-subtle text-light-subtle font-semibold">
+			{children}
+		</label>
 	);
 };
 
