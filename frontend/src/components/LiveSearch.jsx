89450ff9f96e4e057dbe68f9b752783bdf8c7001
Oleg Grunski <oleggr578@gmail.com>
2023-07-29 23:00:24 +0200

    added LiveSearch result item

--- frontend/src/components/LiveSearch.jsx
+++ frontend/src/components/LiveSearch.jsx
@@ -2,9 +2,18 @@ import { commonInputClasses } from '../utils/theme';
 
 const LiveSearch = () => {
 	return (
-		<div>
+		<div className='relative'>
 			<label htmlFor="search"></label>
-			<input type="text" id='search' className={`${commonInputClasses} border-2 rounded p-1 pl-2 pr-2 text-lg`} />
+			<input type="text" id='search' className={`${commonInputClasses} border-2 rounded p-1 pl-2 pr-2 text-lg`} 
+            placeholder='Search profile'
+            />
+            <div className="absolute right-0 left-0 top-10 bg-white dark:bg-secondary shadow-md p-2 max-h-64 overflow-auto space-y-2 mt-1">
+                <p>Hello</p>
+                <p>World</p>
+                <p>How</p>
+                <p>Are</p>
+                <p>You</p>
+            </div>
 		</div>
 	);
 };
