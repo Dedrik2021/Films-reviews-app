875fb521e351f390b3a93a5c13cbd2c90e43c322
Oleg Grunski <oleggr578@gmail.com>
2023-08-08 16:04:28 +0200

    optionaly remove container inside modal

--- frontend/src/components/Modals/ModalContainer.jsx
+++ frontend/src/components/Modals/ModalContainer.jsx
@@ -1,7 +1,17 @@
-const ModalContainer = ({ visible, children, onClose }) => {
+const ModalContainer = ({ visible, ignoreContainer, children, onClose }) => {
 	const handleClick = (e) => {
 		if (e.target.id === 'modal-container') onClose();
 	};
+
+    const renderChildren = () => {
+        if (ignoreContainer) return children
+
+        return (
+            <div className="dark:bg-primary bg-white rounded w-[45rem] h-[40rem] overflow-auto p-2 custom-scroll-bar">
+				{children}
+			</div>
+        )
+    }
 
 	if (!visible) return null;
 
@@ -11,9 +21,7 @@ const ModalContainer = ({ visible, children, onClose }) => {
 			id="modal-container"
 			className="fixed inset-0 dark:bg-white dark:bg-opacity-50 bg-primary bg-opacity-50 backdrop-blur-sm flex items-center justify-center"
 		>
-			<div className="dark:bg-primary bg-white rounded w-[45rem] h-[40rem] overflow-auto p-2 custom-scroll-bar">
-				{children}
-			</div>
+			{renderChildren()}
 		</div>
 	);
 };
--- frontend/src/components/Modals/WritersModal.jsx
+++ frontend/src/components/Modals/WritersModal.jsx
@@ -4,17 +4,19 @@ import ModalContainer from './ModalContainer';
 
 const WritersModal = ({ profiles = [], visible, onClose }) => {
 	return (
-		<ModalContainer visible={visible} onClose={onClose}>
-			<div className="space-y-2">
+		<ModalContainer ignoreContainer visible={visible} onClose={onClose}>
+			<div className="space-y-2 dark:bg-primary bg-white rounded max-w-[45rem] max-h-[40rem] overflow-auto p-2 custom-scroll-bar">
 				{profiles.map(({ id, avatar, name }) => {
 					return (
-						<div className="flex items-center space-x-3" key={id}>
+						<div className="flex space-x-3" key={id}>
 							<img
-								className="w-16 h-16 rounded object-cover"
+								className="w-16 h-16 aspect-square rounded object-cover"
 								src={avatar}
 								alt={name}
 							/>
-							<p className="w-full font-semobold dark:text-white text-primary">{name}</p>
+							<p className="w-full font-semobold dark:text-white text-primary">
+								{name}
+							</p>
 							<button
 								type="button"
 								className="dark:text-white text-primary hover:opacity-80 transition p-2"
