1cd45e725cbea3c8845f3258e89e7e683d152143
Oleg Grunski <oleggr578@gmail.com>
2023-08-08 15:59:12 +0200

    added styles to writers profile

--- frontend/src/components/Modals/WritersModal.jsx
+++ frontend/src/components/Modals/WritersModal.jsx
@@ -1,16 +1,30 @@
+import { AiOutlineClose } from 'react-icons/ai';
+
 import ModalContainer from './ModalContainer';
 
-const WritersModal = ({profiles = [], visible, onClose}) => {
+const WritersModal = ({ profiles = [], visible, onClose }) => {
 	return (
 		<ModalContainer visible={visible} onClose={onClose}>
-			{profiles.map(({ id, avatar, name }) => {
-				return (
-					<div className="flex" key={id}>
-						<img src={avatar} alt={name} />
-						<p>{name}</p>
-					</div>
-				);
-			})}
+			<div className="space-y-2">
+				{profiles.map(({ id, avatar, name }) => {
+					return (
+						<div className="flex items-center space-x-3" key={id}>
+							<img
+								className="w-16 h-16 rounded object-cover"
+								src={avatar}
+								alt={name}
+							/>
+							<p className="w-full font-semobold dark:text-white text-primary">{name}</p>
+							<button
+								type="button"
+								className="dark:text-white text-primary hover:opacity-80 transition p-2"
+							>
+								<AiOutlineClose />
+							</button>
+						</div>
+					);
+				})}
+			</div>
 		</ModalContainer>
 	);
 };
