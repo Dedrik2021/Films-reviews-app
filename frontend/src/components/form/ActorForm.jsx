54d0f3d92074027440e1676768685c799137dd93
Oleg Grunski <oleggr578@gmail.com>
2023-08-22 16:57:45 +0200

    added handling ActorForm state

--- frontend/src/components/form/ActorForm.jsx
+++ frontend/src/components/form/ActorForm.jsx
@@ -1,7 +1,36 @@
+import { useState } from 'react';
+
 import { commonInputClasses } from '../../utils/theme';
 import PosterSelector from '../PosterSelector';
 
+const defaultActorInfo = {
+    name: "",
+    about: "",
+    avatar: null
+}
+
 const ActorForm = ({ title, btnTitle }) => {
+    const [actorInfo, setActorInfo] = useState({...defaultActorInfo})
+    const [selectedAvatarForUI, setSelectedAvatarForUI] = useState("")
+
+    const {name, about} = actorInfo
+
+    const updatePosterForUI = (file) => {
+		const url = URL.createObjectURL(file);
+		setSelectedAvatarForUI(url);
+	};
+
+    const handleChange = ({target}) => {
+        const {value, files, name} = target
+        if (name === "avatar") {
+            const file = files[0]
+            updatePosterForUI(file)
+            return setActorInfo({...actorInfo, avatar: file})
+        }
+
+        setActorInfo({...actorInfo, [name]: value})
+    }
+
 	return (
 		<div className="dark:bg-primary bg-white p-3 w-[35rem] rounded">
 			<div className="flex justify-between items-center mb-3">
@@ -14,21 +43,21 @@ const ActorForm = ({ title, btnTitle }) => {
 				</button>
 			</div>
 			<form className="flex space-x-2">
-				{/* <img
-					src="https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"
-					alt=""
-					className="w-36 h-36 aspect-square object-cover rounded"
-				/> */}
-                <PosterSelector className="w-36 h-36 aspect-square object-cover"/>
+				
+                <PosterSelector name="avatar" onChange={handleChange} selectedPoster={selectedAvatarForUI} className="w-36 h-36 aspect-square object-cover"/>
 				<div className="flex-grow flex flex-col space-y-2">
 					<input
 						type="text"
 						className={`${commonInputClasses} border-b-2`}
 						placeholder="Enter Name..."
+                        name="name" onChange={handleChange}
+                        value={name}
 					/>
 					<textarea
 						placeholder="About"
 						className={`${commonInputClasses} border-b-2 resize-none h-full`}
+                        name="about" onChange={handleChange}
+                        value={about}
 					></textarea>
 				</div>
 			</form>
