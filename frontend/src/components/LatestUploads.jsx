ed0c8ec302129dc333b49acc8f471c99d6cf7108
Oleg Grunski <oleggr578@gmail.com>
2023-09-23 09:05:57 +0200

    refactored time for movie card

--- frontend/src/components/LatestUploads.jsx
+++ frontend/src/components/LatestUploads.jsx
@@ -7,48 +7,56 @@ const LatestUploads = () => {
 				Recent Uploads
 			</h1>
 
-			<MovieListItem />
+			<MovieListItem
+				movie={{
+					poster: 'https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=300&q=80',
+					title: 'lorem ipsum tra tra tra', status: "public", genres: ["Action", "Drama"]
+				}}
+			/>
 		</div>
 	);
 };
 
-const MovieListItem = () => {
+const MovieListItem = ({ movie, onDeleteClick, onEditClick, onOpenClick }) => {
+	const { poster, title, genres = [], status } = movie;
+
 	return (
 		<table className="w-full border-b">
 			<tbody>
 				<tr>
 					<td>
 						<div className="w-24 mr-5">
-							<img
-								className="w-full aspect-video"
-								src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=300&q=80"
-								alt=""
-							/>
+							<img className="w-full aspect-video" src={poster} alt={title} />
 						</div>
 					</td>
 					<td className="w-full">
 						<div>
 							<h2 className="font-semibold text-lg text-primary dark:text-white">
-								Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								{title}
 							</h2>
 							<div className="space-x-1">
-								<span className="text-primary dark:text-white text-xs">Action</span>
-								<span className="text-primary dark:text-white text-xs">Drama</span>
+								{genres.map((g, i) => {
+									return (
+										<span key={g+i} className="text-primary dark:text-white text-xs">
+											{genres.length > 1 ? `${g},` : g}
+										</span>
+									);
+								})}
 							</div>
 						</div>
 					</td>
 					<td>
-						<p className="text-primary dark:text-white mr-2">public</p>
+						<p className="text-primary dark:text-white mr-2">{status}</p>
 					</td>
 					<td>
-						<div className="flex items-center justify-center space-x-2 text-primary dark:text-white text-lg">
-							<button type="button">
+						<div className="flex items-center justify-center space-x-2 text-primary dark:text-white text-lg ">
+							<button className='hover:text-black transition' type="button" onClick={onDeleteClick}>
 								<BsTrash />
 							</button>
-							<button type="button">
+							<button className='hover:text-black  transition ' type="button" onClick={onEditClick}>
 								<BsPencilSquare />
 							</button>
-							<button type="button">
+							<button className=' hover:text-black transition' type="button" onClick={onOpenClick}>
 								<BsBoxArrowUpRight />
 							</button>
 						</div>
