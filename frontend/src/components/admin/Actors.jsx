b96e53a908bfc8dbee492145a309930dfea264e5
Oleg Grunski <oleggr578@gmail.com>
2023-09-25 21:42:48 +0200

    refactored ActorProfile component

--- frontend/src/components/admin/Actors.jsx
+++ frontend/src/components/admin/Actors.jsx
@@ -1,50 +1,77 @@
 import { useState } from 'react';
-import {BsTrash, BsPencilSquare} from 'react-icons/bs'
+import { BsTrash, BsPencilSquare } from 'react-icons/bs';
 
 const Actors = () => {
+	return (
+		<div className="grid grid-cols-4 gap-3 my-5">
+			<ActorProfile profile={{
+				name: "Jhon Doe",
+				avatar: "https://img.freepik.com/premium-photo/life-style-tehnology-travel-concept-bearded-man-wearing-white-tshirt-with-digital-camera-isolated-white-background_118342-60744.jpg?w=360",
+				about: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque voluptate quas veritatis enim debitis! Quisquam repellat temporibus explicabo quia? Porro, accusamus? Ab, consequuntur. Accusamus dolorum quae expedita eius velit nisi?"
+			}} />
+		</div>
+	);
+};
+
+const ActorProfile = ({profile}) => {
 	const [showOptions, setShowOptions] = useState(false);
 
 	const handleOnMouseEnter = () => {
-		setShowOptions(true)
-	}
+		setShowOptions(true);
+	};
 
 	const handleOnMouseLeave = () => {
-		setShowOptions(false)
-	}
+		setShowOptions(false);
+	};
+
+	if (!profile) return null
+	const {avatar, name, about = ""} = profile
 
 	return (
-		<div className="grid grid-cols-4 gap-3 my-5">
-			<div className="bg-white shadow dark:shadow dark:bg-secondary rounded overflow-hidden">
-				<div className="flex cursor-pointer h-20 relative" onMouseEnter={handleOnMouseEnter} onMouseLeave={handleOnMouseLeave}>
-					<img
-						className="w-20 object-cover aspect-square"
-						src="https://img.freepik.com/premium-photo/life-style-tehnology-travel-concept-bearded-man-wearing-white-tshirt-with-digital-camera-isolated-white-background_118342-60744.jpg?w=360"
-						alt=""
-					/>
+		<div className="bg-white shadow dark:shadow dark:bg-secondary rounded overflow-hidden">
+			<div
+				className="flex cursor-pointer h-20 relative"
+				onMouseEnter={handleOnMouseEnter}
+				onMouseLeave={handleOnMouseLeave}
+			>
+				<img
+					className="w-20 object-cover aspect-square"
+					src={avatar}
+					alt={name}
+				/>
 
-					<div className="px-2">
-						<h2 className="text-xl text-primary dark:text-white font-semibold">
-							Jhon Doe
-						</h2>
-						<p className="text-primary dark:text-white">
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
-							recusandae aliquam tempora ut consequuntur quis rem, laudantium
-							voluptatibus nostrum commodi optio voluptates quos
-						</p>
-					</div>
-					{showOptions ? (
-						<div className="absolute inset-0 bg-primary bg-opacity-25 backdrop-blur-sm flex justify-center items-center space-x-5">
-							<button className='p-2 rounded-full bg-white text-primary hover:opacity-80 transition' type='button'>
-								<BsTrash/>
-							</button>
-							<button className='p-2 rounded-full bg-white text-primary hover:opacity-80 transition' type='button'>
-								<BsPencilSquare/>
-							</button>
-							
-						</div>
-					) : null}
+				<div className="px-2">
+					<h2 className="text-xl text-primary dark:text-white font-semibold">{name}</h2>
+					<p className="text-primary dark:text-white">
+						{about.substring(0, 50)}
+						
+					</p>
 				</div>
+				<Options visible={showOptions} />
 			</div>
+		</div>
+	);
+};
+
+const Options = ({ visible, onDeleteClick, onEditClick }) => {
+	if (!visible) return null;
+
+	return (
+		<div className="absolute inset-0 bg-primary bg-opacity-25 backdrop-blur-sm flex justify-center items-center space-x-5">
+			<button
+				className="p-2 rounded-full bg-white text-primary hover:opacity-80 transition"
+				type="button"
+				onClick={onDeleteClick}
+			>
+				<BsTrash />
+			</button>
+			<button
+				onClick={onEditClick}
+				className="p-2 rounded-full bg-white text-primary hover:opacity-80 transition"
+				type="button"
+			>
+				<BsPencilSquare />
+			</button>
 		</div>
 	);
 };
