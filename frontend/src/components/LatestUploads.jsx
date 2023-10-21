03b40500b586824c84dbf733db974b7793a2bf8d
Oleg Grunski <oleggr578@gmail.com>
2023-10-21 11:51:11 +0200

    fetching latest upload for admin

--- frontend/src/components/LatestUploads.jsx
+++ frontend/src/components/LatestUploads.jsx
@@ -1,20 +1,40 @@
-import MovieListItem from "./MovieListItem";
+import { useState, useEffect } from 'react';
+
+import MovieListItem from './MovieListItem';
+import { getMovies } from '../api/movie';
+import { useNotification } from '../hooks';
+
+const pageNo = 0;
+const limit = 5;
 
 const LatestUploads = () => {
-	return (
+	const [movies, setMovies] = useState([]);
+
+	const { updateNotification } = useNotification();
+
+	const fetchLatestUploads = async () => {
+		const { error, movies } = await getMovies(pageNo, limit);
+
+		if (error) return updateNotification('error', error);
+		setMovies([...movies]);
+	};
+
+	useEffect(() => {
+		fetchLatestUploads();
+	}, []);
+
+	return movies.length ? (
 		<div className="bg-white dark:shadow shadow dark:bg-secondary p-5 rounded col-span-2">
 			<h1 className="font-semibold text-2xl mb-2 text-primary dark:text-white">
 				Recent Uploads
 			</h1>
-
-			<MovieListItem
-				movie={{
-					poster: 'https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=300&q=80',
-					title: 'lorem ipsum tra tra tra', status: "public", genres: ["Action", "Drama"]
-				}}
-			/>
+			<div className="space-y-3">
+				{movies.map((movie) => {
+					return <MovieListItem movie={movie} key={movie.id} />;
+				})}
+			</div>
 		</div>
-	);
+	) : null;
 };
 
 export default LatestUploads;
