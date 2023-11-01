6275014dfd255ef04140f0a5d100ecfb70d72766
Oleg Grunski <oleggr578@gmail.com>
2023-11-01 14:59:27 +0100

    grabing query from the URL

--- frontend/src/components/admin/SearchMovies.jsx
+++ frontend/src/components/admin/SearchMovies.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+
 const SearchMovies = () => {
+    const [searchParams] = useSearchParams()
+    const query = searchParams.get('title')
+
+    const searchMovies = (val) => {
+        console.log(val);
+    }
+
+    useEffect(() => {
+        if (query.trim()) searchMovies(query)
+    },[query])
+
     return ( 
         <div>Search movies</div>
     );
