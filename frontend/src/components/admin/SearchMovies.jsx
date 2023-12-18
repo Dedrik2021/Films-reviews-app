cc9696f0a08893e432b720c141c94c020dcf20b9
Oleg Grunski <oleggr578@gmail.com>
2023-12-18 20:34:51 +0100

    recordered result not found

--- frontend/src/components/admin/SearchMovies.jsx
+++ frontend/src/components/admin/SearchMovies.jsx
@@ -1,36 +1,47 @@
-import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
-import { searchMovieForAdmin } from "../../api/movie";
-import { useNotification } from "../../hooks";
-import MovieListItem from "../MovieListItem";
+import { searchMovieForAdmin } from '../../api/movie';
+import { useNotification } from '../../hooks';
+import MovieListItem from '../MovieListItem';
+import NotFoundText from '../NotFoundText';
 
 const SearchMovies = () => {
-    const [searchParams] = useSearchParams()
-    const query = searchParams.get('title')
+	const [searchParams] = useSearchParams();
+	const query = searchParams.get('title');
 
-    const [movies, setMovies] = useState([])
+	const [movies, setMovies] = useState([]);
+	const [resultNotFound, setResultNotFound] = useState(false);
 
-    const {updateNotification} = useNotification()
+	const { updateNotification } = useNotification();
 
-    const searchMovies = async (val) => {
-        const {error, results} = await searchMovieForAdmin(val)
-        if (error) return updateNotification('error', error)
+    
+	const searchMovies = async (val) => {
+        const { error, results } = await searchMovieForAdmin(val);
+		if (error) return updateNotification('error', error);
 
-        setMovies([...results])
-    }
+        if (!results.length) {
+            setResultNotFound(true)
+            return setMovies([])
+        }
+        
+        setResultNotFound(false)
+		setMovies([...results]);
+	};
 
-    useEffect(() => {
-        if (query.trim()) searchMovies(query)
-    },[query])
+	useEffect(() => {
+		if (query.trim()) searchMovies(query);
+	}, [query]);
 
-    return ( 
-        <div className="p-5 space-y-3">
-            {movies.map(movie => {
-                return <MovieListItem movie={movie} key={movie.id} />
-            })}
-        </div>
-    );
-}
+	return (
+		<div className="p-5 space-y-3">
+			<NotFoundText text="Record not found!" visible={resultNotFound} />
+			{!resultNotFound &&
+				movies.map((movie) => {
+					return <MovieListItem movie={movie} key={movie.id} />;
+				})}
+		</div>
+	);
+};
 
 export default SearchMovies;
