c47956e98c616e629e1515bc65123ccd0784b5d2
Oleg Grunski <oleggr578@gmail.com>
2023-12-26 02:10:36 +0100

    created review validation && addreview func

--- backend/controllers/review.mjs
+++ backend/controllers/review.mjs
@@ -1,4 +1,37 @@
-const addReview = (req, res) => {
+import { isValidObjectId } from "mongoose"
+
+import { sendError } from "../utils/helper.mjs"
+import Movie from '../models/movie.mjs'
+import Review from '../models/review.mjs'
+
+const addReview = async (req, res, next) => {
+    const {movieId} = req.params
+    const {content, rating} = req.body
+    const userId = req.user._id
+    let movie, isAlreadyReview
+    
+    if (isValidObjectId(movieId)) return sendError(res, 'Invalid movie!')
+
+    try {
+        movie = await Movie.findOne({_id: movieId, status: "public"})
+    } catch(err) {
+        return next(sendError(req, "Movie not found!", 404))
+    }
+
+    if (!movie) return sendError(req, "Movie not found!", 404)
+
+    try {
+        isAlreadyReview = await Review.findOne({
+            owner: userId,
+            parentMovie: movie._id
+        })
+    } catch(err) {
+        return next(sendError(res, "Something went wrong!"))
+    }
+
+    if (isAlreadyReview) return sendError(req, "Invalid request, review is already their!")
+
+
 
 }
 
