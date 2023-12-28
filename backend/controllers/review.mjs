afd638ea8c71c7c778544b12b0ef47174e10bb9b
Oleg Grunski <oleggr578@gmail.com>
2023-12-28 12:09:51 +0100

    created reviewupdate backend function

--- backend/controllers/review.mjs
+++ backend/controllers/review.mjs
@@ -52,6 +52,36 @@ const addReview = async (req, res, next) => {
     } catch(err) {
         return next(sendError(res, 'Review not has been saved.'))
     }
+
+    res.json({message: "Your review has been added!"})
 }
 
-export {addReview}
+const updateReview = async (req, res, next) => {
+    const {reviewId} = req.params
+    const {content, rating} = req.body
+    const userId = req.user._id
+    let review
+    
+    if (isValidObjectId(reviewId)) return sendError(res, 'Invalid review ID!')
+
+    try {
+        review = await Review.findOne({_id: reviewId, owner: userId})
+    } catch(err) {
+        return next(sendError(req, "Review not found!", 404))
+    }
+
+    if (!review) return sendError(req, "Review not found!", 404)
+
+    review.content = content
+    review.rating = rating
+
+    try {
+        await review.save()
+    } catch(err) {
+        return next(sendError(res, 'Review not has been saved.'))
+    }
+
+    res.json({message: "Your review has been updated!"})
+}
+
+export {addReview, updateReview}
--- backend/routes/review.mjs
+++ backend/routes/review.mjs
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { addReview } from '../controllers/review.mjs';
+import { addReview, updateReview } from '../controllers/review.mjs';
 import { uploadImage } from '../middlewares/multer.mjs';
 import { validateRatings, validate } from '../middlewares/validator.mjs';
 import { isAdmin, isAuth } from '../middlewares/auth.mjs';
@@ -8,5 +8,6 @@ import { isAdmin, isAuth } from '../middlewares/auth.mjs';
 const router = Router();
 
 router.post('/add/:movieId', isAuth, validateRatings, validate, addReview);
+router.patch('/:reviewId', isAuth, validateRatings, validate, updateReview);
 
 export default router;
