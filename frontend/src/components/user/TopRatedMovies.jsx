3bc414449fb39f58e09097b7bcc0c50f2a7d0c71
Oleg Grunski <oleggr578@gmail.com>
2024-01-14 00:29:10 +0100

    created GridContainer component & created responsive grid columns

--- frontend/src/components/user/TopRatedMovies.jsx
+++ frontend/src/components/user/TopRatedMovies.jsx
@@ -1,9 +1,15 @@
-const TopRatedMovies = () => {
-    return ( 
-        <div className="grid grid-cols-5 gap-3">
+import GridContainer from '../GridContainer';
 
-        </div>
-    );
-}
+const TopRatedMovies = () => {
+	return (
+		<GridContainer>
+			{Array(5)
+				.fill('')
+				.map((_, i) => {
+					return <div className="p-5 bg-red-200" key={i}></div>;
+				})}
+		</GridContainer>
+	);
+};
 
 export default TopRatedMovies;
