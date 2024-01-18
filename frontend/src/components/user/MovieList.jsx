99f6595b78f2a87b50fd775e733f3c35ba435856
Oleg Grunski <oleggr578@gmail.com>
2024-01-18 22:21:38 +0100

    refactored movie list component

---
+++ frontend/src/components/user/MovieList.jsx
@@ -0,0 +1,50 @@
+import { AiFillStar } from 'react-icons/ai';
+
+import GridContainer from '../GridContainer';
+
+const trimTitle = (text = '') => {
+    if (text.length <= 20) return text;
+    return text.substring(0, 20) + '..';
+};
+
+const MovieList = ({ title, movies = [] }) => {
+
+	if (!movies.length) return null;
+
+	return (
+		<div>
+			<h1 className="text-2xl mb-5 dark:text-white text-secondary font-semibold">{title}</h1>
+			<GridContainer>
+				{movies.map((movie) => {
+					return <ListItem id={movie.id} movie={movie} />;
+				})}
+			</GridContainer>
+		</div>
+	);
+};
+
+const ListItem = ({ movie }) => {
+	return (
+		<div>
+			<img className="aspect-video object-cover" src={movie.poster} alt={movie.title} />
+			<h2
+				className="text-lg dark:text-white text-secondary font-semibold"
+				title={movie.title}
+			>
+				{trimTitle(movie.title)}
+			</h2>
+			{movie.reviews?.ratingAvg ? (
+				<p className="text-highlight dark:text-highlight-dark flex items-center space-x-1">
+					<span>{movie.reviews?.ratingAvg}</span>
+					<AiFillStar />
+				</p>
+			) : (
+				<p className="text-highlight dark:text-highlight-dark flex items-center space-x-1">
+					No Reviews
+				</p>
+			)}
+		</div>
+	);
+};
+
+export default MovieList;
--- frontend/src/components/user/TopRatedMovies.jsx
+++ frontend/src/components/user/TopRatedMovies.jsx
@@ -1,9 +1,8 @@
 import { useState, useEffect } from 'react';
-import { AiFillStar } from 'react-icons/ai';
 
-import GridContainer from '../GridContainer';
 import { useNotification } from '../../hooks';
 import { getTopRatedMovies } from '../../api/movie';
+import MovieList from './MovieList';
 
 const TopRatedMovies = () => {
 	const [movies, setMovies] = useState([]);
@@ -20,42 +19,7 @@ const TopRatedMovies = () => {
 		fetchMovies();
 	}, []);
 
-	const trimTitle = (text = '') => {
-		if (text.length <= 20) return text;
-		return text.substring(0, 20) + '..';
-	};
-
-	return (
-		<GridContainer>
-			{movies.map((movie) => {
-				return (
-					<div key={movie.id}>
-						<img
-							className="aspect-video object-cover"
-							src={movie.poster}
-							alt={movie.title}
-						/>
-						<h1
-							className="text-lg dark:text-white text-secondary font-semibold"
-							title={movie.title}
-						>
-							{trimTitle(movie.title)}
-						</h1>
-						{movie.reviews?.ratingAvg ? (
-							<p className="text-highlight dark:text-highlight-dark flex items-center space-x-1">
-								<span>{movie.reviews?.ratingAvg}</span>
-								<AiFillStar />
-							</p>
-						) : (
-							<p className="text-highlight dark:text-highlight-dark flex items-center space-x-1">
-								No Reviews
-							</p>
-						)}
-					</div>
-				);
-			})}
-		</GridContainer>
-	);
+	return <MovieList title="Viewers choice (Movies)" movies={movies} />;
 };
 
 export default TopRatedMovies;
