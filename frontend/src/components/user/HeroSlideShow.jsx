09f7e634b215e01a7705edc749568c7504ffc104
Oleg Grunski <oleggr578@gmail.com>
2024-01-22 22:12:07 +0100

    rendered slide

--- frontend/src/components/user/HeroSlideShow.jsx
+++ frontend/src/components/user/HeroSlideShow.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
+
 const HeroSlideShow = () => {
+    const [slide, setSlide] = useState({})
+
     return ( 
-        <div>Hero slide show</div>
+        <div className="w-full flex">
+            <div className="w-4/5 aspect-video">
+                <img src={slide.poster} alt="" />
+            </div>
+            <div className="w-1/5 aspect-video bg-red-300"></div>
+        </div>
     );
 }
 
