ecf8967d564d21f9a9b53a57e07ff66051a8559f
Oleg Grunski <oleggr578@gmail.com>
2024-01-24 22:14:06 +0100

    created slide show controller

--- frontend/src/components/user/HeroSlideShow.jsx
+++ frontend/src/components/user/HeroSlideShow.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import {AiOutlineDoubleLeft, AiOutlineDoubleRight} from "react-icons/ai"
 
 import { useNotification } from '../../hooks';
 import { getLatestUploads } from '../../api/movie';
@@ -23,12 +24,28 @@ const HeroSlideShow = () => {
 
 	return (
 		<div className="w-full flex">
-			<div className="w-4/5 aspect-video">
+			<div className="w-4/5 aspect-video relative">
 				<img src={slide.poster} alt="" />
+                <SlideShowController/>
 			</div>
 			<div className="w-1/5 aspect-video bg-red-300"></div>
 		</div>
 	);
 };
+
+const SlideShowController = ({onNextClick, onPrevClick}) => {
+    const btnClass = 'bg-primary rounded border-2 text-white text-xl p-2 outline-none'
+
+    return (
+        <div className="absolute top-1/2 -translate-y-1/2 w-full flex items-center justify-between px-2 ">
+            <button type='button' className={btnClass} onClick={onPrevClick}>
+                <AiOutlineDoubleLeft/>
+            </button>
+            <button type='button' className={btnClass} onClick={onNextClick}>
+                <AiOutlineDoubleRight/>
+            </button>
+        </div>
+    )
+}
 
 export default HeroSlideShow;
