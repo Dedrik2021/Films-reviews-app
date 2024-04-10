54febec1430a627343ff3947c7ae67cc0f68a2f3
Oleg Grunski <oleggr578@gmail.com>
2024-04-11 00:05:39 +0200

    added textarea and submit btn

--- frontend/src/components/Modals/AddRatingModal.jsx
+++ frontend/src/components/Modals/AddRatingModal.jsx
@@ -1,12 +1,14 @@
-import ModalContainer from './ModalContainer';
 import {AiOutlineStar} from 'react-icons/ai'
+
+import ModalContainer from './ModalContainer';
+import SubmitBtn from '../form/SubmitBtn'
 
 
 const ratings = new Array(10).fill("")
 
 const AddRatingModal = () => {
 	return <ModalContainer visible ignoreContainer>
-        <div className="p-5 dark:bg-primary bg-white rounded">
+        <div className="p-5 dark:bg-primary bg-white rounded space-y-3">
             <div className="text-highlight dark:text-highlight-dark flex items-center">
                 {ratings.map((_, i) => {
                     return (
@@ -14,6 +16,9 @@ const AddRatingModal = () => {
                     )
                 })}
             </div>
+            <textarea className='w-full h-24 border-2 p-2 dark:text-white text-primary rounded outline-none bg-transparent resize-none'></textarea>
+
+            <SubmitBtn value="Rate This Movie"/>
         </div>
     </ModalContainer>;
 };
