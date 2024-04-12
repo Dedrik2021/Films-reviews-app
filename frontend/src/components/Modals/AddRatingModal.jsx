1bb9228f5051131574a6442444efb29eec672726
Oleg Grunski <oleggr578@gmail.com>
2024-04-12 22:54:10 +0200

    added different icons for selected ratings

--- frontend/src/components/Modals/AddRatingModal.jsx
+++ frontend/src/components/Modals/AddRatingModal.jsx
@@ -1,4 +1,5 @@
-import {AiOutlineStar} from 'react-icons/ai'
+import {AiOutlineStar, AiFillStar} from 'react-icons/ai'
+import { useState } from 'react';
 
 import ModalContainer from './ModalContainer';
 import SubmitBtn from '../form/SubmitBtn'
@@ -7,14 +8,28 @@ import SubmitBtn from '../form/SubmitBtn'
 const ratings = new Array(10).fill("")
 
 const AddRatingModal = () => {
+    const [selectedRatings, setSelectedRatings] = useState([...ratings])
+
+    const handleMouseEnter = (index) => {
+
+    }
+
 	return <ModalContainer visible ignoreContainer>
         <div className="p-5 dark:bg-primary bg-white rounded space-y-3">
-            <div className="text-highlight dark:text-highlight-dark flex items-center">
+            <div className="text-highlight dark:text-highlight-dark flex items-center relative">
                 {ratings.map((_, i) => {
                     return (
-                        <AiOutlineStar key={i} size={24}/>
+                        <AiOutlineStar onMouseEnter={() => handleMouseEnter(i)} key={i} size={24} className='cursor-pointer'/>
                     )
                 })}
+
+                <div className="flex items-center absolute top-1/2 -translate-y-1/2">
+                    {selectedRatings.map((_, i) => {
+                        return (
+                        <AiFillStar onMouseEnter={() => handleMouseEnter(i)} key={i} size={24} className='cursor-pointer'/>
+                    )
+                    })}
+                </div>
             </div>
             <textarea className='w-full h-24 border-2 p-2 dark:text-white text-primary rounded outline-none bg-transparent resize-none'></textarea>
 
