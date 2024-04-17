d825e64525842de0f4839ee7dbfc3b739e9298ac
Oleg Grunski <oleggr578@gmail.com>
2024-04-17 21:22:47 +0200

    fixed the NaN bug

--- frontend/src/components/user/SingleMovie.jsx
+++ frontend/src/components/user/SingleMovie.jsx
@@ -35,7 +35,7 @@ const SingleMovie = () => {
 		if (movieId) fetchMovie();
 	}, [movieId]);
 
-	const convertReviewCount = (count) => {
+	const convertReviewCount = (count = 0) => {
 		if (count <= 999) return count;
 
 		return parseFloat(count / 1000).toFixed(2) + 'k';
